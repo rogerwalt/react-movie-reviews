5fc563e1463b9559a7c3d0675a3c9c7167132bc0
Roger Walt <roger.walt@ti8m.ch>
2018-03-05 10:48:42 +0100

    Poster base url as constant

--- src/components/MovieListItem/MovieListItem.js
+++ src/components/MovieListItem/MovieListItem.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
+import { POSTER_BASE_URL } from '../../config/constants';
+
 export default function MovieListItem(props) {
-  const imageUrl = `https://image.tmdb.org/t/p/w500${props.movie.poster_path}`;
+  const imageUrl = `${POSTER_BASE_URL}${props.movie.poster_path}`;
 
   return  <div className="cell"><img src={imageUrl} />{props.movie.title}</div>;
 }
