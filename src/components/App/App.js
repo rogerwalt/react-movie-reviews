f9a3d7233d266e4fad82c98e02abe17d328e6042
Roger Walt <roger.walt@ti8m.ch>
2018-03-05 10:10:43 +0100

    MovieList component begin

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from '../../assets/images/logo.svg';
-import '../../styles/App.css';
+import '../../styles/_app.scss';
+
+import MovieList from '../MovieList/MovieList';
+
+import mockData from '../../__fixtures__/movies.json';
+
+const movies = mockData.results;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="grid-container">
+        <MovieList movies={movies} />
       </div>
     );
   }
